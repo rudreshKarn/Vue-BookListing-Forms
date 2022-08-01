c71d2a9890b7c321e4193a3c953d990c77cf6270
rudreshKarn <62277931+rudreshKarn@users.noreply.github.com>
2022-08-01 21:20:26 +0530

    Module 5

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
+    <input type="text" placeHolder="Search Books" v-model="searchInput">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
-    <br><hr>
+    <br>
+    <hr>
     <h2>Filtered Books By Ownership</h2>
     <select v-model="holding">
-    <option v-for="filter in filters"> {{filter}} </option>    
+      <option v-for="filter in filters"> {{ filter }} </option>
     </select>
     <ul>
       <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
     </ul>
-    <br><hr>
+    <br>
+    <hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
@@ -29,18 +32,27 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed"},
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought"},
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed"}
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed" }
       ],
       filters: ["bought", "borrowed"],
-      holding:"bought"
+      holding: "bought",
+      searchInput: "",
     };
   },
-  computed:{
-    filteredBooks(){
-      return _.filter(this.books,["ownership", this.holding]);
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      }
+
+      return _.filter(this.books, searchFilter);
     }
+
   },
   components: {
     BookItem,
@@ -48,7 +60,7 @@ export default {
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading , ownership: bookData.ownership});
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading, ownership: bookData.ownership });
     }
   }
 };
@@ -59,6 +71,7 @@ h1,
 h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
